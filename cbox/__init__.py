53a2b3dcc80bd16cc029aa385d4f2487cc0db880
shmuelamar <shmulikamar@gmail.com>
2017-08-31 02:41:14 +0300

    fix version

--- cbox/__init__.py
+++ cbox/__init__.py
@@ -1,5 +1,5 @@
 __description__ = 'create unix-style commands from your python functions.'
-__version__ = 'v0.1.0'
+__version__ = '0.1.0'
 __author__ = 'Shmuel Amar'
 __url__ = 'https://github.com/shmuelamar/cbox'
 __license__ = 'MIT'
